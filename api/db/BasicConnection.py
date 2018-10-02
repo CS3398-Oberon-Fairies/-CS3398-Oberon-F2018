9251cb40f6c3cd87e8b515d3bd07d32cdcbe0395
Patrick Gruber <p@gruber.io>
2018-10-01 19:08:59 -0500

    API to DB

---
+++ api/db/BasicConnection.py
@@ -0,0 +1,28 @@
+
+import mysql.connector
+import _mysql
+
+class BasicConnection:
+
+	_conn = None
+
+	# ==========================================================================
+	def __init__(self, username, password, host, db):
+		self._conn = mysql.connector.connect(
+			user=username,
+			password=password,
+			host=host,
+			database=db
+		)
+
+	# ==========================================================================
+	def getResult(self, query):
+		cur = self._conn.cursor()
+		cur.execute(query)
+		return [it[0] for it in cur.description], list(cur)
+
+	# ==========================================================================
+	def execute(self, query):
+		cur = self._conn.cursor()
+		cur.execute(query)
+		self._conn.commit()
